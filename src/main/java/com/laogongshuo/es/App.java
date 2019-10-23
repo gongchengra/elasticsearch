46b2811b4d352ed121961d3759013ead2c888fd4
龚成 <gongcheng749@pingan.com.cn>
2019-10-23 16:10:27 +0800

    add count api example

--- src/main/java/com/laogongshuo/es/App.java
+++ src/main/java/com/laogongshuo/es/App.java
@@ -2,12 +2,16 @@ package com.laogongshuo.es;
 
 import java.io.IOException;
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.core.CountRequest;
+import org.elasticsearch.client.core.CountResponse;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.ShardSearchFailure;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
@@ -26,55 +30,71 @@ public class App {
     "city",
     "state"
   };
+  private static RestHighLevelClient client =
+      new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+  private static SearchRequest searchRequest = new SearchRequest("bank");
+
+  public static void printResponse(SearchResponse searchResponse) {
+    if (searchResponse.getHits().getTotalHits().value > 0) {
+      System.out.println(searchResponse.getHits().getTotalHits());
+      for (SearchHit hit : searchResponse.getHits()) {
+        System.out.println("Match: ");
+        for (String fetchField : FETCH_FIELDS) {
+          System.out.println(" - " + fetchField + " " + hit.getSourceAsMap().get(fetchField));
+        }
+      }
+    } else {
+      System.out.println("No results matching the criteria.");
+    }
+  }
 
   public static void matchAll() throws IOException {
-    RestHighLevelClient client =
-        new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
-    SearchRequest searchRequest = new SearchRequest("bank");
     SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
     searchSourceBuilder.query(QueryBuilders.matchAllQuery());
     searchRequest.source(searchSourceBuilder);
-
     SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
-    if (searchResponse.getHits().getTotalHits().value > 0) {
-      System.out.println(searchResponse.getHits().getTotalHits());
-      for (SearchHit hit : searchResponse.getHits()) {
-        System.out.println("Match: ");
-        for (String fetchField : FETCH_FIELDS) {
-          System.out.println(" - " + fetchField + " " + hit.getSourceAsMap().get(fetchField));
-        }
-      }
-    } else {
-      System.out.println("No results matching the criteria.");
-    }
-    client.close();
+    printResponse(searchResponse);
   }
 
   public static void search() throws IOException {
-    RestHighLevelClient client =
-        new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
-    SearchRequest searchRequest = new SearchRequest("bank");
     SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
     sourceBuilder.query(QueryBuilders.termQuery("account_number", "49"));
     searchRequest.source(sourceBuilder);
-
     SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
-    if (searchResponse.getHits().getTotalHits().value > 0) {
-      System.out.println(searchResponse.getHits().getTotalHits());
-      for (SearchHit hit : searchResponse.getHits()) {
-        System.out.println("Match: ");
-        for (String fetchField : FETCH_FIELDS) {
-          System.out.println(" - " + fetchField + " " + hit.getSourceAsMap().get(fetchField));
-        }
+    printResponse(searchResponse);
+  }
+
+  private static CountRequest countRequest = new CountRequest("bank");
+  public static void count() throws IOException {
+    SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+    searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+    countRequest.source(searchSourceBuilder);
+    CountResponse countResponse = client.count(countRequest, RequestOptions.DEFAULT);
+    long count = countResponse.getCount();
+    System.out.println(count);
+    RestStatus status = countResponse.status();
+    System.out.println(status);
+    Boolean terminatedEarly = countResponse.isTerminatedEarly();
+    System.out.println(terminatedEarly);
+    int totalShards = countResponse.getTotalShards();
+    System.out.println(totalShards);
+    int skippedShards = countResponse.getSkippedShards();
+    System.out.println(skippedShards);
+    int successfulShards = countResponse.getSuccessfulShards();
+    System.out.println(successfulShards);
+    int failedShards = countResponse.getFailedShards();
+    System.out.println(failedShards);
+    if (failedShards > 0) {
+      for (ShardSearchFailure failure : countResponse.getShardFailures()) {
+        System.out.println(failure);
       }
-    } else {
-      System.out.println("No results matching the criteria.");
     }
-    client.close();
   }
 
   public static void main(String[] args) throws IOException {
     matchAll();
     search();
+    count();
+    client.close();
   }
 }
