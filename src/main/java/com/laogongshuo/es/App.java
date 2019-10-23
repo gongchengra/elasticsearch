ea1dc7ef740f7d5faa0a2ce61ac5a6998d08f114
龚成 <gongcheng749@pingan.com.cn>
2019-10-23 17:28:14 +0800

    added index function and added google java formatter

--- src/main/java/com/laogongshuo/es/App.java
+++ src/main/java/com/laogongshuo/es/App.java
@@ -1,15 +1,21 @@
 package com.laogongshuo.es;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.http.HttpHost;
-import org.elasticsearch.client.core.CountRequest;
-import org.elasticsearch.client.core.CountResponse;
+import org.elasticsearch.action.DocWriteResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.ShardSearchFailure;
+import org.elasticsearch.action.support.replication.ReplicationResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.core.CountRequest;
+import org.elasticsearch.client.core.CountResponse;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.SearchHit;
@@ -65,6 +71,7 @@ public class App {
   }
 
   private static CountRequest countRequest = new CountRequest("bank");
+
   public static void count() throws IOException {
     SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
     searchSourceBuilder.query(QueryBuilders.matchAllQuery());
@@ -91,10 +98,46 @@ public class App {
     }
   }
 
+  public static void index() throws IOException {
+    Map<String, Object> jsonMap = new HashMap<>();
+    jsonMap.put("account_number", "1000");
+    jsonMap.put("balance", "59999");
+    jsonMap.put("firstname", "Alan");
+    jsonMap.put("lastname", "Hans");
+    jsonMap.put("age", "32");
+    jsonMap.put("gender", "M");
+    jsonMap.put("address", "Shenzhen Fuyong Street");
+    jsonMap.put("employer", "Pingan Group");
+    jsonMap.put("email", "alan@gmail.com");
+    jsonMap.put("city", "Shenzhen");
+    jsonMap.put("state", "GD");
+    IndexRequest indexRequest = new IndexRequest("bank").id("1000").source(jsonMap);
+    IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+    String index = indexResponse.getIndex();
+    System.out.println(index);
+    String id = indexResponse.getId();
+    System.out.println(id);
+    if (indexResponse.getResult() == DocWriteResponse.Result.CREATED) {
+      System.out.println("created.");
+    } else if (indexResponse.getResult() == DocWriteResponse.Result.UPDATED) {
+      System.out.println("updated.");
+    }
+    ReplicationResponse.ShardInfo shardInfo = indexResponse.getShardInfo();
+    if (shardInfo.getTotal() != shardInfo.getSuccessful()) {}
+
+    if (shardInfo.getFailed() > 0) {
+      for (ReplicationResponse.ShardInfo.Failure failure : shardInfo.getFailures()) {
+        String reason = failure.reason();
+        System.out.println(reason);
+      }
+    }
+  }
+
   public static void main(String[] args) throws IOException {
     matchAll();
     search();
     count();
+    index();
     client.close();
   }
 }
